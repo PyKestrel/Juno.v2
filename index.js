cbbd3c30fe94e7de01d9ee5f995e27c4a770969d
Ariel Torres <42158489+PyKestrel@users.noreply.github.com>
2023-04-01 19:10:05 -0400

    Added Multiple Voice Functions

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 const { Client, Intents, Integration } = require("discord.js");
 const { clientId, token } = require("./config.json");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice");
+const {
+  joinVoiceChannel,
+  getVoiceConnection,
+  AudioPlayerStatus,
+  AudioResource,
+  AudioPlayer,
+  createAudioPlayer,
+} = require("@discordjs/voice");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const fs = require("node:fs");
-const path = require("node:path");
+const { EmbedBuilder } = require("discord.js");
+const ytdl = require("ytdl-core");
+const yts = require("yt-search");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -49,6 +57,9 @@ const commands = [
             .setRequired(true)
         )
     ),
+  new SlashCommandBuilder()
+    .setName("disconnect")
+    .setDescription("Disconnect Juno from Voice Channel"),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(token);
@@ -60,9 +71,68 @@ rest
 
 // Voice Channel Functions
 
-async function createChannelConnection() {}
+/*
 
-async function deleteChannelConnection() {}
+The CreateChannelConnection function is passed the interaction object when a user initiates the /play slash command.
+Using the interaction object we will retrieve information related to the VoiceChannel that the initiator is in and pass that information to the joinVoiceChannel function.
+
+*/
+async function createChannelConnection(interaction) {
+  const channel = interaction.member.voice.channel;
+  try {
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+    return 1;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+}
+
+/*
+
+The DeleteChannelConnection function is passed the interaction object when a user initiates the /disconnect slash command.
+Using the interaction object we will retrieve the active voice connection within the guild and then destroy it thus disconnecting the bot.
+
+*/
+
+async function deleteChannelConnection(interaction) {
+  const channel = interaction.member.voice.channel;
+  const connection = getVoiceConnection(channel.guild.id);
+  try {
+    connection.destroy();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/*
+
+The subscribeChannelConnection is the next step in the Voice Channel connection and is in charge of streaming the actual aduio to the voice channel.
+The function is in charge of building the audioPLayer object that will hold our audio stream.
+
+*/
+
+async function subscribeChannelConnection(interaction) {
+  let audioPlayer = new AudioPlayer();
+  const channel = interaction.member.voice.channel;
+  const connection = getVoiceConnection(channel.guild.id);
+  const subscription = connection.subscribe(audioPlayer);
+}
+
+/*
+
+The ytdlSearch function is used to take a search string and find an associated youtube audio.
+
+*/
+async function ytdlSearch(){
+    
+}
+
+
 
 // These are the interactions with the slash commands
 client.on("interactionCreate", async (interaction) => {
@@ -76,17 +146,24 @@ client.on("interactionCreate", async (interaction) => {
       await interaction.followUp("Pong again!");
       break;
     case "play":
-      // Use Interaction Object To Get Which Sub-Command Was Used And Retrive Its Value
+      // Use Interaction Object To Get Which Sub-Command Was Used And Retrive Its Name & Value
       let type = interaction.options.data[0].name;
       if (type == "search") {
         interaction.reply(
           `Searching For ${interaction.options.data[0].options[0].value}`
         );
         let commandOption = interaction.options.data[0].options[0].value;
+        createChannelConnection(interaction);
       } else if (type == "url") {
         interaction.reply(`Fetching Your Audio!`);
         let commandOption = interaction.options.data[0].options[0].value;
+        createChannelConnection(interaction);
       }
+      break;
+    case "disconnect":
+      await interaction.reply("Disconnecting...");
+      await deleteChannelConnection(interaction);
+      await interaction.deleteReply();
       break;
     default:
       break;
@@ -95,7 +172,3 @@ client.on("interactionCreate", async (interaction) => {
 
 // Login to Discord with your client's token
 client.login(token);
-
-/*
-    CommENtinG by RichRArd
-*/
